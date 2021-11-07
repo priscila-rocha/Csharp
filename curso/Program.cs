c735209c70f5c2d40d4e49e58bcd7f536e5d027c
Priscila Rocha <priscilar.developer@gmail.com>
2021-11-06 22:02:37 -0300

    Add files via upload

---
+++ curso/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Globalization;
+
+namespace curso
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double x = 10.35784;
+            int idade = 37;
+            string nome = "Priscila";
+            char sexo = 'F';
+            string sexoExtenso = String.Empty;
+
+            if (sexo == 'F')
+            {
+                sexoExtenso = "feminino";
+            }
+            else
+            {
+                sexoExtenso = "masculino";
+            }
+
+            Console.WriteLine("Hello World");
+            Console.Write("Bom dia! ");
+            Console.Write("Até mais!");
+
+            Console.WriteLine();
+            Console.WriteLine($"\nO resultado sem configuração de número de casas decimais é: {x}.");
+            Console.WriteLine($"O resultado com configuração de duas casas decimais é: {x.ToString("F2")}.");
+            Console.WriteLine($"O resultado com configuração de quatro casas decimais é: {x.ToString("F4")}.");
+            Console.WriteLine($"O resultado com configuração regional é: {x.ToString("F2", CultureInfo.InvariantCulture)}");
+
+            Console.WriteLine();
+            Console.WriteLine($"A paciente {nome}, tem {idade} anos e seu sexo é {sexoExtenso}.");
+
+        }
+    }
+}
