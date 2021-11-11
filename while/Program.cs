4686702155e92916f3932e2f08165cb433752454
Priscila Rocha <priscilar.developer@gmail.com>
2021-11-11 12:20:51 -0300

    Add files via upload

---
+++ while/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace while_soma
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int entrada = int.Parse(Console.ReadLine());
+            int soma = 0;
+
+            while (entrada != 0)
+            {
+                soma += entrada;
+                entrada = int.Parse(Console.ReadLine());                
+            }
+            Console.WriteLine($"A soma de todos os numeros é {soma}");
+        }
+
+    }
+}
