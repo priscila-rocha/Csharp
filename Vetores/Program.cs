baec47a3b6e039dc4f553d2786be4f7f7fa9db18
Priscila Rocha <priscilar.developer@gmail.com>
2021-11-11 12:25:09 -0300

    Add files via upload

---
+++ Vetores/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Globalization;
+
+namespace Vetores
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int tamanhoVetor = int.Parse(Console.ReadLine());
+            double[] numeros = new double[tamanhoVetor];
+
+            for (int i = 0; i < tamanhoVetor; i++){
+                numeros[i] = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            }
+
+            Console.WriteLine();
+
+            foreach(double n in numeros)//tipo de n é o mesmo tipo do vetor
+            {
+                //na impressão chama a variavel n, pois ela representa o elemento dentro do vetor
+                Console.WriteLine(n.ToString("F1", CultureInfo.InvariantCulture));
+            }
+
+        }
+    }
+}
