b6ced822832c0a81f3140267d5ef2a6c8bc6f650
Priscila Rocha <priscilar.developer@gmail.com>
2021-11-11 12:21:25 -0300

    Add files via upload

---
+++ for/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace for_soma
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int contador = int.Parse(Console.ReadLine());
+            int soma = 0;
+
+            for (int i = 0; i < contador; i++){
+                int numeros = int.Parse(Console.ReadLine());
+                soma += numeros;
+            }
+            Console.WriteLine(soma);
+        }
+    }
+}
