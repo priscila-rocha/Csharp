5086eafc5e76b27a0fa8475f48644cdad48dc23b
Priscila Rocha <priscilar.developer@gmail.com>
2021-11-14 23:15:56 -0300

    Add files via upload

---
+++ Retangulo/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Globalization;
+
+namespace Retangulo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Retangulo retangulo = new Retangulo();
+
+           Console.WriteLine("Digite a Largura e Altura do Retangulo: ");
+           retangulo.Largura = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+           retangulo.Altura = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+          Console.WriteLine($"AREA = {retangulo.Area().ToString("F2", CultureInfo.InvariantCulture)})");
+
+
+        }
+    }
+}
