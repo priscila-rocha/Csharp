324afe790f026f2196e6f867efabe3a53e7081b2
Priscila Rocha <priscilar.developer@gmail.com>
2021-11-14 23:14:05 -0300

    Add files via upload

---
+++ TrianguloRefatorado/TrianguloRefatorado.cs
@@ -0,0 +1,24 @@
+using System;
+
+namespace TrianguloRefatorado
+{
+    public class Triangulo
+    {
+        public double A;
+        public double B;
+        public double C;
+
+        public double Area(){
+
+            // double p = (A + B + C) / 2.0;
+            // double raiz = Math.Sqrt(p * (p - A) * (p - B) * (p - C));
+            // return raiz;
+
+            //ou pode ser feito da seguinte forma:
+
+            double p = (A + B + C) / 2.0;
+            return Math.Sqrt(p * (p - A) * (p - B) * (p - C));
+            
+        }
+    }
+}
